90821829bd438086112f5f1a5ca91af05ee51973
kouya17 <17383696+kouya17@users.noreply.github.com>
2019-10-19 15:05:08 +0100

    Update: function

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import re
 
 app = Flask(__name__)
 
+def get_target_num(response):
+    return re.search(r'(?<==)\d+', response).group()
+
 @app.route("/", methods=["GET"])
 def get_shoot():
     if request.method == "GET":
@@ -12,14 +15,14 @@ def get_shoot():
         url_target = 'http://192.168.100.125'
         headers = {}
         req = urllib.request.Request(url_target)
+        target_num = '0'
         with urllib.request.urlopen(req) as res:
             res_html = res.read().decode('utf-8')
-            print('score_site res=' + res_html)
-        return "Hello World!"
+            print('score_site res = ' + res_html)
+            target_num = get_target_num(res_html)
+            print('target_num = ' + target_num)
+        return target_num
 
 if __name__ == "__main__":
     app.run("0.0.0.0")
-
-def get_target_num(response):
-	return re.search(r'(?<==)\d+', s).group())
 
