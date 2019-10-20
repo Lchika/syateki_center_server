295bdd2e38be7fe11903df870287df62ec6511fc
kouya17 <17383696+kouya17@users.noreply.github.com>
2019-10-20 22:36:30 +0900

    Update: test soundplayer

--- test/test_soundplayer.py
+++ test/test_soundplayer.py
@@ -2,18 +2,26 @@ import sys
 import pathlib
 import unittest
 import subprocess
+import threading
 parent_dir = str(pathlib.Path(__file__).parent.parent.resolve())
 sys.path.append(parent_dir)
 from common.c_messenger import Sender, Receiver
 from device.a_soundplayer import ApiSoundPlayer
 
 
+def run_task():
+    command = ['python', str(pathlib.Path(__file__).parent.parent.resolve()) + '/device/t_soundplayer.py']
+    res = subprocess.call(command)
+    print('res = ' + res)
+
+
+task_thread = threading.Thread(target=run_task)
+task_thread.start()
+
+
 class TestSoundPlayer(unittest.TestCase):
     def setUp(self):
         print('TestSoundPlayer setup')
-        command = ['python', str(pathlib.Path(__file__).parent.parent.resolve()) + '/device/t_soundplayer.py']
-        res = subprocess.call(command)
-        print('res = ' + res)
         self.soundplayer = ApiSoundPlayer()
     
     def test_play_file(self):
